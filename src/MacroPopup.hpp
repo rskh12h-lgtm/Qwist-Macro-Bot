b07da4eab49c98c0e75bf53dd2a4f72d5727e385
rskh12h-lgtm <rskh12h@gmail.com>
2026-08-26 11:07:24 +0200

    Update MacroPopup.hpp

--- src/MacroPopup.hpp
+++ src/MacroPopup.hpp
@@ -22,6 +22,14 @@ protected:
     CCMenuItemSpriteExtra* m_playBtn = nullptr;
     ButtonSprite* m_loopSprite = nullptr;
     CCMenuItemSpriteExtra* m_loopBtn = nullptr;
+    ButtonSprite* m_autoSaveSprite = nullptr;
+    CCMenuItemSpriteExtra* m_autoSaveBtn = nullptr;
+
+    // Deplacement (drag) de la fenetre
+    bool m_dragging = false;
+    CCPoint m_dragTouchStart;
+    CCPoint m_dragNodeStart;
+    float m_dragZoneHeight = 0.f;
 
     bool init();
     void refresh();
@@ -33,7 +41,12 @@ protected:
     void onSave(CCObject*);
     void onLoad(CCObject*);
     void onLoop(CCObject*);
+    void onAutoSave(CCObject*);
     void onSoon(CCObject*);
+
+    bool ccTouchBegan(CCTouch* touch, CCEvent* event) override;
+    void ccTouchMoved(CCTouch* touch, CCEvent* event) override;
+    void ccTouchEnded(CCTouch* touch, CCEvent* event) override;
 
 public:
     static MacroPopup* create();
